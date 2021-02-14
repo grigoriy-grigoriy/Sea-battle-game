c49dff86d7b27a21bd2d30187197fb09d3cd9930
materikzentr@gmail.com <materikzentr@gmail.com>
2021-02-14 19:46:17 +0200

    обновление класса

--- Comp.cs
+++ Comp.cs
@@ -1,8 +1,33 @@
 ﻿using System;
-
-public class Class1
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace WindowsFormsApp9
 {
-	public Class1()
-	{
-	}
+    public class Comp   // комп. управление в игре 
+    {
+        // компьютер
+        public int[,] myMap = new int[Form1.mapSize, Form1.mapSize];
+        // игрок
+        public int[,] enemyMap = new int[Form1.mapSize, Form1.mapSize];
+
+        public Button[,] myButtons = new Button[Form1.mapSize, Form1.mapSize];
+        public Button[,] enemyButtons = new Button[Form1.mapSize, Form1.mapSize];
+
+        public Comp(int[,] myMap, int[,] enemyMap, Button[,] myButtons, Button[,] enemyButtons)
+        {
+            this.myMap = myMap;
+            this.enemyMap = enemyMap;
+            this.enemyButtons = enemyButtons;
+            this.myButtons = myButtons;
+        }
+        
+
+
+
+
+    }
 }
